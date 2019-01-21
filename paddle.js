56554b76c04e6ce02d7545ceef37e5f763e1bf31
Atharva <asphadnis1@gmail.com>
2019-01-21 14:01:33 +0530

    Add switch case to paddle.js.

--- paddle.js
+++ paddle.js
@@ -1,14 +1,39 @@
 
 class Paddle {
     constructor(x, y, width, height, color) {
-        this.x = paddleX;
+        this.x = x;
         this.y = y;
+        this.vel = paddleVel;
         this.width = width;
         this.height = height;
         this.color = color;
+        this.thickness = 3;
     }
 
     draw(){
-        colorRect(paddleX, paddleY, paddleWidth, paddleHeight, this.color);
+        hollowRect(this.x, this.y, this.width, this.height, this.color, this.thickness);
+    }
+
+    update(key){
+        switch(key) {
+            case 37 : //Left
+                if(this.x > 2){
+                this.x -= this.vel;
+                }
+                break;
+            case 39 : //Right
+                if(this.x < canvas.width - this.width - 2){
+                this.x += this.vel;
+                }
+                break;
+            case 27 : //Escape
+                if(isPlaying){
+                    isPlaying = false;
+                }
+                else if(isPlaying == false){
+                    isPlaying = true;
+                }
+                break;
+        }
     }
 }
